ed589f6275e1fb543970bd7d24f86abfb2a717b1
Nelson Alexander <alexbnelson@yahoo,com>
2018-10-27 20:42:05 +0100

    Additions to file selector

--- diptych.publisher/src/components/MainPage/PublisherForm/ChapterCreator.js
+++ diptych.publisher/src/components/MainPage/PublisherForm/ChapterCreator.js
@@ -4,18 +4,35 @@ import '../../../bootstrap.css';
 
 
 class ImageSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.fileInput = React.createRef();
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        alert(
+            `Selected file - ${
+            this.fileInput.current.files[0].name
+            }`
+        );
+    }
 
     render() {
         return (
-            <div>
-            <input type="file"> Choose Image</input>
-            <Cropper src='http://fengyuanchen.github.io/cropper/img/picture.jpg' />
-            <h3>Text: </h3>
-            <textarea rows="4" cols="50">
-                </textarea>
-                </div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Upload file:
+          <input type="file" ref={this.fileInput} />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         );
     }
 }
+
+);
+
 
 export default ImageSelector
